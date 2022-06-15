b8117cb65992fb281a70b0eb44add2c6d9d24035
Restyled.io <commits@restyled.io>
2022-06-15 15:15:36 +0000

    Restyled by black

--- algo.py
+++ algo.py
@@ -4,14 +4,13 @@ import numpy as np
 from decimal import *
 
 
-def rndFloat(min, max, decimals, size, addchars=''):
+def rndFloat(min, max, decimals, size, addchars=""):
     # Reference: https://stackoverflow.com/questions/2891790/how-to-pretty-print-a-numpy-array-without-scientific-notation-and-with-given-pre
     decimals = "{: 0." + str(decimals) + "f}"
-    result = np.random.random(size) * (max - min) + \
-        min  # (math min-max formula)
-    np.set_printoptions(formatter={'float': decimals.format}, suppress=True)
+    result = np.random.random(size) * (max - min) + min  # (math min-max formula)
+    np.set_printoptions(formatter={"float": decimals.format}, suppress=True)
     if not addchars:
-        return (result)
+        return result
     else:
         result = [str(arr) + str(addchars) for arr in result]
         return result
