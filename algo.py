cd9f65eabca610cc094fde2997b83f21a6a04290
Restyled.io <commits@restyled.io>
2022-06-15 15:15:34 +0000

    Restyled by autopep8

--- algo.py
+++ algo.py
@@ -1,16 +1,21 @@
-import random, decimal, numpy as np
+import random
+import decimal
+import numpy as np
 from decimal import *
 
-def rndFloat(min, max, decimals, size, addchars=''): 
-    #Reference: https://stackoverflow.com/questions/2891790/how-to-pretty-print-a-numpy-array-without-scientific-notation-and-with-given-pre
-    decimals = "{: 0." + str(decimals) + "f}" 
-    result = np.random.random(size) * (max - min) + min #(math min-max formula)
-    np.set_printoptions(formatter={'float': decimals.format},suppress=True)
+
+def rndFloat(min, max, decimals, size, addchars=''):
+    # Reference: https://stackoverflow.com/questions/2891790/how-to-pretty-print-a-numpy-array-without-scientific-notation-and-with-given-pre
+    decimals = "{: 0." + str(decimals) + "f}"
+    result = np.random.random(size) * (max - min) + \
+        min  # (math min-max formula)
+    np.set_printoptions(formatter={'float': decimals.format}, suppress=True)
     if not addchars:
         return (result)
     else:
         result = [str(arr) + str(addchars) for arr in result]
         return result
+
 
 def rndChoice(chars, size=1):
     return np.random.choice(chars, size=size, replace=True)
