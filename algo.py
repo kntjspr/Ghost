a1e43d74a458868fb43b5566fc312f4b73bcb2d7
Restyled.io <commits@restyled.io>
2022-06-15 15:15:45 +0000

    Restyled by yapf

--- algo.py
+++ algo.py
@@ -8,7 +8,8 @@ import numpy as np
 def rndFloat(min, max, decimals, size, addchars=""):
     # Reference: https://stackoverflow.com/questions/2891790/how-to-pretty-print-a-numpy-array-without-scientific-notation-and-with-given-pre
     decimals = "{: 0." + str(decimals) + "f}"
-    result = np.random.random(size) * (max - min) + min  # (math min-max formula)
+    result = np.random.random(size) * (max -
+                                       min) + min  # (math min-max formula)
     np.set_printoptions(formatter={"float": decimals.format}, suppress=True)
     if not addchars:
         return result
