9a4d096c2c830da1ab59ea2489e6f28fa8fa1553
Restyled.io <commits@restyled.io>
2022-07-20 19:52:08 +0000

    Restyled by autopep8

--- range.py
+++ range.py
@@ -1,11 +1,17 @@
-import random, decimal, numpy as np
+import random
+import decimal
+import numpy as np
 from decimal import *
-def rndFloat(min, max, decimals, size, addchars=''): 
-    #Reference: https://stackoverflow.com/questions/2891790/how-to-pretty-print-a-numpy-array-without-scientific-notation-and-with-given-pre
-    #size = number of iterations, addchars = covert float to string and concatenate
-    decimals = "{: 0." + str(decimals) + "f}" # formatter={'float': {: 0." + str(decimals) + "f}.format}
-    result = np.random.random(size) * (max - min) + min #(math min-max formula neural network)
-    np.set_printoptions(formatter={'float': decimals.format},suppress=True)
+
+
+def rndFloat(min, max, decimals, size, addchars=''):
+    # Reference: https://stackoverflow.com/questions/2891790/how-to-pretty-print-a-numpy-array-without-scientific-notation-and-with-given-pre
+    # size = number of iterations, addchars = covert float to string and concatenate
+    # formatter={'float': {: 0." + str(decimals) + "f}.format}
+    decimals = "{: 0." + str(decimals) + "f}"
+    # (math min-max formula neural network)
+    result = np.random.random(size) * (max - min) + min
+    np.set_printoptions(formatter={'float': decimals.format}, suppress=True)
     if not addchars:
         return (result)
     else:
@@ -18,4 +24,3 @@ print(test[0], test[1], test[2])
 f = open("demofile2.txt", "a")
 f.write(str(test))
 f.close()
-
